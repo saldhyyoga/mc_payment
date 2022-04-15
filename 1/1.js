d73e4c95548f669d3ac0c6651af8cfd79ad41676
saldhyyoga <saldhyy@gmail.com>
2022-04-15 21:23:05 +0700

    setup database and add more validation sudoku

--- 1/1.js
+++ 1/1.js
@@ -1,5 +1,6 @@
 function ValidSudoku(testcase, board){
   let items = 0;
+  let totalItems = 0;
   const messages = [];
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
@@ -10,12 +11,17 @@ function ValidSudoku(testcase, board){
         return 'Invalid';
       }
       items++;
+      totalItems += board[i][j];
     }
   }
 
   let boardLength = board.length;
   if ( items !== (boardLength * 9) ){
     return 'Invalid';
+  }
+
+  if ( totalItems !== ( testcase * 405 )) {
+    return  'Invalid';
   }
 
   for (let x = 0; x < testcase; x++) {
@@ -26,7 +32,7 @@ function ValidSudoku(testcase, board){
 };
 
 const param1 = [
-  [5, 3, 4, 6, 7, 8, 9, 1, 9],
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
   [6, 7, 2, 1, 9, 5, 4, 3, 8],
   [1, 9, 8, 3, 4, 2, 5, 6, 7],
   [8, 5, 9, 7, 6, 1, 4, 2, 3],
---
+++ 2/helpers/database.js
@@ -0,0 +1,14 @@
+const sqlite = require('sqlite3').verbose();
+const dbsource = "db.sqlite";
+
+const Connection = new sqlite.Database(dbsource, (err) => {
+  if (err) {
+    console.log(err.message);
+    throw err;
+  } else {
+    console.log('Connected to the sqlite')
+
+  }
+})
+
+module.exports = Connection;
